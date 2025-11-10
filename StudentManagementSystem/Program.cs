e89f84e7622500f880b16e4d781cfb5a8a7cf703
Alme1da04 <joaoaansaloni@gmail.com>
2025-11-09 22:20:17 -0300

    Substituindo projeto antigo pelo novo

--- StudentManagementSystem/Program.cs
+++ StudentManagementSystem/Program.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 using System.Dynamic;
 using Microsoft.VisualBasic;
-// unused imports, but can be needed in other machines
 
 using Microsoft.Data.SqlClient;
 
